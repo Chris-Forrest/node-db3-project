51136369e463459ca47b6edab4304a2c95808203
Chris Forrest <redrider_cr@yahoo.com>
2020-08-26 17:02:57 -0400

    3 gets post a scheme and delete a scheme working edit a scheme not working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,6 +3,12 @@ const db = require("../data/dbConfig");
 module.exports = {
     find,
     findById,
+    findSteps,
+    add,
+    update,
+    remove,
+    addStep,
+
 };
 
 function find(){
@@ -13,4 +19,30 @@ function findById(id){
     return db("schemes")
     .where({ id })
     .first()
+};
+
+function findSteps(id){
+    return db("steps")
+};
+
+function add(data){
+    return db("schemes")
+    .insert(data, "id")
+};
+
+function update(changes,id){
+   return db("schemes")
+   .where({ id })
+   .update(changes)
+   .first()
+};
+
+function remove(id){
+    return db("schemes")
+    .where({ id })
+    .del()
+};
+
+function addStep(){
+
 };
